c4702757076ae6b2da74ce1fc1834abf9c39bea6
mgcerati <mcerati@gmail.com>
2022-01-27 12:31:46 -0300

    Se agrega leyenda carrito vacio

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { useCartContext } from '../context/cartContext'
 
 
@@ -11,17 +12,28 @@ const Cart = () => {
         <div>
             <h1>Carrito de compra</h1>
             <div>
-                {cartList.map((producto) =>(
+                {cartList.length === 0 ? (
+                    <div>
+                        <h2>No agregaste nada al carrito</h2>
+                        <Link to="/">
+                            <button>Ir al catalogo</button>
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                {cartList?.map((producto) =>(
                     <div key={producto.id}>
                         <div>{producto.name}</div>
                         <div>Cantidad: {producto.counter}</div>
                         <div>Precio: {producto.price}</div>
                         <button onClick={()=>eliminarProducto(producto.id)}>Eliminar Producto</button>
                     </div>
-                ))} 
+                ))}
+                <button onClick={vaciarCarrito}>Vaciar Lista de compras</button>
+                </> 
+                )}
             </div>
             <br />
-            <button onClick={vaciarCarrito}>Vaciar Lista de compras</button>
         </div>
         </>
         
